d4a388b64f87ba431167bacd86c656ab8efa5bf3
chea-young <63410509+chea-young@users.noreply.github.com>
2021-07-10 12:09:29 +0900

    init : init back_end

---
+++ back_end/app.js
@@ -0,0 +1,13 @@
+var express = require('express'); 
+var app = express(); 
+
+const port = 3000;
+
+app.listen(port, function() {
+    console.log('app listening ${port}')
+})
+
+app.get('/', function(req, res){
+    res.status(200).json({'msg' : 'Hello World'})
+  })
+
