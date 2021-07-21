123438e6be06d5ee7787f9f1597bd298eea48f48
chea-young <63410509+chea-young@users.noreply.github.com>
2021-07-21 22:24:22 +0900

    feat: add phone number

--- back_end/create_db.sql
+++ back_end/create_db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE backend;
 USE backend;
-CREATE TABLE Student (id VARCHAR(255) PK, passwd VARCHAR(255), name VARCHAR(255), grade INT(255));
+CREATE TABLE Student (id VARCHAR(255) PK, passwd VARCHAR(255), name VARCHAR(255), grade INT(255), phone_number INT(255));
 CREATE TABLE Post (title VARCHAR(255), body VARCHAR(255), show TINYINT(1), student_id VARCHAR(255), date DATETIME DEFAULT NOW());
 CREATE TABLE Subject (title VARCHAR(255), contents VARCHAR(255), type VARCHAR(255)  INDEX classid (title));
 CREATE TABLE LessonRate(class_id INT(255), student_id VARCHAR(255), complete TinyINT(1));
