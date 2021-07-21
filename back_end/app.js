5342bb01035a744c57d6cc6b168e9334e6acce64
Hm-source <swo8852@gmail.com>
2021-07-21 17:55:16 +0900

    feat: seperate router and set ejs

--- back_end/app.js
+++ back_end/app.js
@@ -1,13 +1,23 @@
-var express = require('express'); 
-var app = express(); 
+"use strict";
+// module
+var express = require('express');
+var app = express();
 
+// port number
 const port = 3000;
 
-app.listen(port, function() {
-    console.log('app listening ${port}')
-})
+// routing
+const home = require("./routes/home");
 
-app.get('/', function(req, res){
-    res.status(200).json({'msg' : 'Hello World'})
-})
+//app setting
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
+//use -> 미들웨어 등록해주는 메서드
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", home);
+
+
+
+module.exports = app;
