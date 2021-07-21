6c3d1baaa9bb6490b316e749051633a9a084059c
chea-young <63410509+chea-young@users.noreply.github.com>
2021-07-21 21:22:53 +0900

    feat: modify sql

--- back_end/create_db.sql
+++ back_end/create_db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE backend;
 USE backend;
-CREATE TABLE User (id VARCHAR(30) PK, passwd VARCHAR(16), name VARCHAR(255), grade INT(20));
+CREATE TABLE User (id VARCHAR(25) PK, passwd VARCHAR(255), name VARCHAR(255), grade INT(20));
 CREATE TABLE BaseClass (contant VARCHAR(255), title VARCHAR(255), INDEX num (title));
 CREATE TABLE HardClass (contant VARCHAR(255), title VARCHAR(255), INDEX num (title));
-CREATE TABLE LessonRate(id VARCHAR(30) PK, complete TINYINT(1), class_num INT(255), level VARCHAR(4));
+CREATE TABLE LessonRate(id VARCHAR(255) PK, complete TINYINT(1), class_num INT(255), level VARCHAR(4));
