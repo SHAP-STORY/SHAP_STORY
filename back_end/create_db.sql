9b732e295be96b588019a5714b800af9ffceae4a
Hm-source <swo8852@gmail.com>
2021-07-24 13:18:23 +0900

    fix: modify db table
    phone_number ->char(11)

--- back_end/create_db.sql
+++ back_end/create_db.sql
@@ -5,7 +5,7 @@ CREATE TABLE Student (
   `passwd` VARCHAR(255) NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   `grade` INT(255) NOT NULL,
-  `phone_number` INT(255) NOT NULL,
+  `phone_number` CHAR(11) NOT NULL,
   PRIMARY KEY(id)
 ) CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 CREATE TABLE Post (
