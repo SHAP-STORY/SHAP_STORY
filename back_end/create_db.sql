8bb17bab3cb1da0f685fcb9e2ddbed86d147bb9f
Hm-source <swo8852@gmail.com>
2021-07-24 12:56:12 +0900

    fix: modify db table

--- back_end/create_db.sql
+++ back_end/create_db.sql
@@ -1,6 +1,27 @@
 CREATE DATABASE story;
 USE story;
-CREATE TABLE Student (`id` VARCHAR(255), `passwd` VARCHAR(255), `name` VARCHAR(255), `grade` INT(255), `phone_number` INT(255));
-CREATE TABLE Post (title VARCHAR(255), body VARCHAR(255), show TINYINT(1), student_id VARCHAR(255), date DATETIME DEFAULT NOW());
-CREATE TABLE Subject (title VARCHAR(255), contents VARCHAR(255), type VARCHAR(255)  INDEX classid (title));
-CREATE TABLE LessonRate(class_id INT(255), student_id VARCHAR(255), complete TinyINT(1));
+CREATE TABLE Student (
+  `id` VARCHAR(255) NOT NULL,
+  `passwd` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `grade` INT(255) NOT NULL,
+  `phone_number` INT(255) NOT NULL,
+  PRIMARY KEY(id)
+) CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+CREATE TABLE Post (
+  title VARCHAR(255) NOT NULL,
+  body VARCHAR(255) NOT NULL,
+  show TINYINT(1),
+  student_id VARCHAR(255),
+  date DATETIME DEFAULT NOW()
+) CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+CREATE TABLE Subject (
+  title VARCHAR(255),
+  contents VARCHAR(255),
+  type VARCHAR(255) INDEX classid (title)
+) CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+CREATE TABLE LessonRate(
+  class_id INT(255),
+  student_id VARCHAR(255),
+  complete TinyINT(1)
+) CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
