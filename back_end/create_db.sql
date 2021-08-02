885d218cea628e9eba51806ff41942ca99b9da50
hyo-min <64147798+Hm-source@users.noreply.github.com>
2021-08-02 21:40:07 +0900

    fix: modify db table

--- back_end/create_db.sql
+++ back_end/create_db.sql
@@ -11,17 +11,17 @@ CREATE TABLE Student (
 CREATE TABLE Post (
   title VARCHAR(255) NOT NULL,
   body VARCHAR(255) NOT NULL,
-  show TINYINT(1),
-  student_id VARCHAR(255),
+  show TINYINT(1) NOT NULL,
+  student_id VARCHAR(255) NOT NULL,
   date DATETIME DEFAULT NOW()
 ) CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 CREATE TABLE Subject (
-  title VARCHAR(255),
+  title VARCHAR(255) NOT NULL,
   contents VARCHAR(255),
   type VARCHAR(255) INDEX classid (title)
 ) CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 CREATE TABLE LessonRate(
-  class_id INT(255),
-  student_id VARCHAR(255),
-  complete TinyINT(1)
+  class_id INT(255) NOT NULL,
+  student_id VARCHAR(255) NOT NULL,
+  complete TinyINT(1) NOT NULL
 ) CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
