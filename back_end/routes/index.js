5624adbe2e0675e6a6b1945e0c2602a989fb7f66
chea-young <63410509+chea-young@users.noreply.github.com>
2021-08-07 12:58:02 +0900

    feat: add post url

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -5,8 +5,10 @@ const router = express.Router();
 
 const home = require('./home/index');
 const posts = require('./posts/index');
+const mypage = require("./routes/mypage");
 
 router.use('/home', home);
 router.use('/posts', posts);
+app.use('/mypage', mypage);
 
 module.exports = router;
