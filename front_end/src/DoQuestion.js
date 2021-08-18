6a53c5d73a74730a5fffe54908f5ac48e6e340d8
Heeeon <51069854+bbjoite09@users.noreply.github.com>
2021-08-18 22:42:10 +0900

    feat : question page - view 완성

--- front_end/src/DoQuestion.js
+++ front_end/src/DoQuestion.js
@@ -9,11 +9,15 @@ const DoQuestion = () => {
     return (
         <Background>
             <Modal>
-                <Align>
-                    <h1>질문하기</h1>
-                    <Input></Input>
-                    <Input style={{height: "365px"}}></Input>
-                </Align>
+                <AlignCol>
+                    <Title>질문하기</Title>
+                    <Link to="./questions">
+                        <ButtonOut>X</ButtonOut>
+                    </Link>
+                    <Input placeholder="제목을 입력해주세요"/>
+                    <Input placeholder="질문을 작성해주세요" style={{height: "330px"}}/>
+                    <SaveButton>저장</SaveButton>
+                </AlignCol>
             </Modal>
         </Background>
     );
@@ -40,14 +44,40 @@ const Modal = styled.div`
     height: 635px;
 `;
 
-const Align = styled.div`
+const AlignCol = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: center;
     item-align: center;
 `;
 
+const Title = styled.h1`
+    margin-top: 0;
+    position: absolute;
+    top: 7%;
+    left: 50%;
+    transform: translate(-50%, 0%);
+`;
+
+const ButtonOut = styled.button`
+    margin-top: 0;
+    position: absolute;
+    top: 7%;
+    left: 90%;
+    font-weight: bold;
+    font-size: 30px;
+    background-color: #00ff0000;
+    border-radius: 15px;
+    border: 0;
+    
+    &: hover{
+        background-color: #EAEAEA;
+        color: black;
+    }
+`;
+
 const Input = styled.input`
+    font-size: 17px;
     margin-top: 15px;
     text-align: left;
     padding-left: 30px;
@@ -57,6 +87,27 @@ const Input = styled.input`
     border: 0;
     outline: none;
     background-color: #EAEAEA;
+`;
+
+const SaveButton = styled.button`
+    position: absolute;
+    top: 85%;
+    left: 50%;
+    transform: translate(-50%, 0);
+    
+    margin-top: 20px;
+    font-size: 17px;
+    font-weight: 550;
+    background-color: #3F3D56;
+    border: 0;
+    color: white;
+    width: 110px;
+    height: 48px;
+    border-radius: 30px;
+    &: hover{
+        background-color: #dadbdb;
+        color: black;
+    }
 `;
 
 export default DoQuestion;
