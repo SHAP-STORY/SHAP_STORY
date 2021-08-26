fb43a3bed00311fb3a2e3ee9a7d2bda168d21523
chea-young <63410509+chea-young@users.noreply.github.com>
2021-08-26 16:25:43 +0900

    feat: init the server about the lesson

--- back_end/routes/index.js
+++ back_end/routes/index.js
@@ -6,9 +6,11 @@ const router = express.Router();
 const home = require('./home/index');
 const posts = require('./posts/index');
 const mypage = require("./mypage/index");
+const lesson = require("./lesson/index");
 
 router.use('/api/home', home);
 router.use('/posts', posts);
 router.use('/api/mypage', mypage);
+router.use('api/lesson', lesson);
 
 module.exports = router;
---
+++ back_end/routes/lesson/index.js
@@ -0,0 +1,23 @@
+"use strict";
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const multer = require("multer");
+const upload = multer({ dest: "./upload" });
+//user_info
+var user_info = require('../varient');
+
+// mysql
+const db = require('../../db/database');
+
+router.get('/basic', function(req, res, next) {
+    db.query('SELECT class_id, complete FROM LessonRate where Student_id= ? ',[user_info[1]], function (error, results, fields) {
+        if (error) {
+            console.log(error);
+        }
+        res.send(results);
+    });
+});
+
+
+module.exports = router; // 꼭 넣어주기 아니면 에러가 난다!
